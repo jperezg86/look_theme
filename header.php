f3b77ba677213d723123ecc6b737c0765cd6d3f2
Ricardo Mares <riqrdo@outlook.com>
2019-03-21 14:55:25 -0600

    ajuste de meta description

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<!-- <meta name="description" content="<?php bloginfo('description'); ?>"> -->
 		<meta name="p:domain_verify" content="579cebb9bf858f873b59814c24b5c7aa"/>
 
 		<?php wp_head(); ?>
